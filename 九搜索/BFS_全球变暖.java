8d4ef8d21f58af0105efa1f080eabddd21bba1f6
AKBoom20 <614501755@qq.com>
2023-03-02 16:53:48 +0800

    amend BFS_全球变暖 with two nested for,while still TLE

--- 九搜索/BFS_全球变暖.java
+++ 九搜索/BFS_全球变暖.java
@@ -1,9 +1,7 @@
 package 九搜索;
 
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BFS_全球变暖 {
@@ -22,24 +20,23 @@ public class BFS_全球变暖 {
 		for (int i = 0; i < n; i++)
 			map[i] = scanner.next().toCharArray();
 		scanner.close();
-
-		Deque<Node> queue = new LinkedList<>();
-		queue.offer(new Node(0, 0));
-		while (!queue.isEmpty()) {
-			Node node = queue.poll();
-			// 如果发现一片还没有检查的陆地 开始检查这片陆地
-			if (map[node.x][node.y] == '#' && flag[node.x][node.y] == 0) {
-				flag[node.x][node.y] = 1;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] == '.' || flag[i][j] == 1)
+					continue;
+				// 如果发现一片还没有检查的陆地 开始检查这片陆地
+				flag[i][j] = 1;
+				int landcnt = 0, floodedcnt = 0;
+				Node node = new Node(i, j);
 				Deque<Node> landDeque = new LinkedList<>();
-				List<Node> landList = new ArrayList<>();
-				landDeque.add(node);
+				landDeque.offer(node);
 				while (!landDeque.isEmpty()) {
 					node = landDeque.poll();
+					landcnt++;
 					flag[node.x][node.y] = 1;
-					landList.add(node);
-					for (int i = 0; i < 4; i++) {
-						int nx = node.x + dx[i];
-						int ny = node.y + dy[i];
+					for (int k = 0; k < 4; k++) {
+						int nx = node.x + dx[k];
+						int ny = node.y + dy[k];
 						if (nx < n && ny < n && nx >= 0 && ny >= 0 && flag[nx][ny] == 0) {
 							if (map[nx][ny] == '.')// 如果检查到一片水 陆地被淹没
 								node.f = 1;
@@ -47,30 +44,14 @@ public class BFS_全球变暖 {
 								landDeque.offer(new Node(nx, ny));
 						}
 					}
+					if (node.f == 1)
+						floodedcnt++;
 				}
 				// 检查陆地是否被淹没了
-				int flooded = 1;
-				for (Node node2 : landList)
-					if (node2.f == 0)
-						flooded = 0;
-				if (flooded == 1)
+				if (floodedcnt == landcnt)
 					ans++;
 			}
-			// 如果发现一片水域 继续查找四周
-			else if (map[node.x][node.y] == '.' && flag[node.x][node.y] == 0) {
-				flag[node.x][node.y] = 1;
-				for (int i = 0; i < 4; i++) {
-					int nx = node.x + dx[i];
-					int ny = node.y + dy[i];
-					if (nx < n && ny < n && nx >= 0 && ny >= 0 && flag[nx][ny] == 0)
-						if (map[nx][ny] == '#')
-							queue.add(new Node(nx, ny, 1));
-						else
-							queue.add(new Node(nx, ny));
-				}
 
-			}
-		}
 		System.out.println(ans);
 	}
 }
